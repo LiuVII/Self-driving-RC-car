e357eceb547b0237eccb381febcba9590a363fec
Robotics <robotics@sierra-test.local>
2017-05-14 02:32:25 -0700

    updated labelling script to reflect current usage

--- label.py
+++ label.py
@@ -1,31 +1,38 @@
 import serial
-import time, datetime, glob, os, re
+import time, datetime, glob, os, re, sys
 import subprocess
 
-# port = '/dev/cu.usbmodem14211'
+argc = len(sys.argv)
+if argc > 1:
+        device = sys.argv[1]
+else:
+	#sometimes device is 14211
+        device = '/dev/cu.usbmodem14311'
 
-# ser = serial.Serial(port, 9600,timeout=5)
-ot = 0
+if argc > 2:
+        baud = int(sys.argv[2])
+else:
+        baud = 9600
 
-# go to data dir
+if argc > 3:
+        fps = float(sys.argv[3])
+else:
+        fps = 5.
 
-while True:
-	ts = time.time()
-	st = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S.%f')
-	# line = ser.readline().strip()
-	if ts - ot > 0.2:
-		test = subprocess.check_output("ls data | tail -n1", shell=True)
-		# print(test)
-		test = test.decode("utf-8").strip()
-		data = re.search("(IMG.*\.jpg)$", test)
-		if data:
-			f = data.group(1)
-		else:
-			f = -1
-	#	if glob.iglob('data/*.jpg'):
-	#		f = max(glob.iglob('data/*.jpg'), key=os.path.getctime)
-	#	else:
-	#		f = -1
-		# print("%s, %s, %s"%(f, st, line))
-		print("%s, %s"%(f, st))
-		ot = ts
+with serial.Serial(device, baud) as ser:
+        ot = 0
+        while True:
+                ts = time.time()
+                st = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S.%f')
+                line = ser.readline().strip().decode("utf-8")
+                if re.search("^[0-9]*\.?[0-9]+$", line):
+                        if float(line) and ts - ot > 1./fps:
+                                test = subprocess.check_output("ls -l data | tail -n1", shell=True)
+                                test = test.decode("utf-8").strip()
+                                data = re.search("(IMG.*\.jpg)$", test)
+                                if data:
+                                        f = data.group(1)
+                                else:
+                                        f = -1
+                                print("%s, %s, %s"%(f, st, line))
+                                ot = ts
