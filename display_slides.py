5fe42f818d549cc3354042785a8b9f1ccf9628c3
Mikhail Filipchuk <mfilipch@e1z2r9p8.42.us.org>
2017-05-14 21:38:49 -0700

    with training model

--- display_slides.py
+++ display_slides.py
@@ -11,9 +11,15 @@ import pandas as pd
 
 
 # Data folder name annd path
-dir_name = "data"
-outdir = "outdir"
-full_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+dir_name = "data/"
+outdir = "outdir/"
+full_path = os.path.dirname(os.path.realpath(sys.argv[0])) + "/"
+outdir = full_path + outdir
+if not os.path.exists(outdir):
+	os.mkdir(outdir)
+data_dir = "./model_data/"
+if not os.path.exists(data_dir):
+	os.mkdir(data_dir)
 
 # Image flow params
 width = 320
@@ -25,11 +31,10 @@ st_min_val = 0.48
 st_mid_val = 1.51
 st_max_val = 2.90
 st_curr = 0
-st_right_rng = st_mid_val - st_min_val
-st_left_rng = st_max_val - st_mid_val
+
 
 # Internal params
-fps = 5
+fps = 60
 tries = 0
 ind = 0
 t0 = 0.0
@@ -40,24 +45,35 @@ max_angle = pi / 4.0
 
 # Read csv from file into dataframe
 df = pd.read_csv("out.csv", names=['img_name', 'datetime', 'steering'])
+# sa_lst = [[],[]]
+sa_lst = []
+sample_length = len(df)
 
-while tries < 10:
+# Set steering boundaries
+if min(df['steering']) < st_min_val:
+	st_min_val = min(df['steering'])
+if max(df['steering']) > st_max_val:
+	st_max_val = min(df['steering'])
+st_right_rng = st_mid_val - st_min_val
+st_left_rng = st_max_val - st_mid_val
+
+
+while tries < 10 and ind < sample_length:
 	# Measure time to keep certain drawing speed
 	t1 = time.time()
 	if t1 - t0 > 1.0 / fps:
 		
 		st_curr = float(df['steering'].iloc[ind])
 		if st_curr > 0 and st_curr < st_mid_val:
-			angle = ((st_min_val - st_curr) / st_right_rng) * max_angle
+			angle = ((st_curr - st_mid_val) / st_right_rng) * max_angle
 		elif st_curr > 0:
-			angle = ((st_max_val - st_curr) / st_left_rng) * max_angle
+			angle = ((st_curr - st_mid_val) / st_left_rng) * max_angle
 		else:
 			angle = 0
 		# img_name = subprocess.check_output("ls " + dir_name + " | tail -n1", shell=True).decode("utf-8").strip()
-		# img_name = full_path + "/"  + dir_name + "/" + img_name
 		# str_num = str(num)
-		# img_name = full_path + "/"  + dir_name + "/" + "IMG_" + (5 - len(str_num))*'0' + str_num + ".jpg"
-		img_name = full_path + "/"  + dir_name + "/" + df['img_name'].iloc[ind]
+		# img_name = full_path + dir_name + "IMG_" + (5 - len(str_num))*'0' + str_num + ".jpg"
+		img_name = full_path + dir_name + df['img_name'].iloc[ind]
 		print(img_name)
 		
 		img = cv2.imread(img_name, 1)
@@ -77,12 +93,19 @@ while tries < 10:
 			cv2.line(img,(int(width / 2 - x_shift), int(height - y_shift)),(width // 2, height),(255,0,0),5)
 			# shift = int(sqrt(r_sq - (r - num % d) ** 2))
 			# cv2.line(img,(int(width / 2 - r + num % d), int(height - shift)),(width // 2, height),(255,0,0),5)
+			
+			## Print img_name and angle to STDOUT and save to the list
+			print("%s, %.2f" % (df['img_name'].iloc[ind], angle))
+			# sa_lst[0].append(df['img_name'].iloc[ind])
+			# sa_lst[1].append(angle)
+			sa_lst.append([df['img_name'].iloc[ind], angle])
 			## Show combined image
 			str_ind = str(ind)
-			cv2.imwrite(outdir + "/" + "IMG_" + (5 - len(str_ind))*'0' + strq_ind + ".jpg", img)
+			# cv2.imwrite(outdir + "IMG_" + (5 - len(str_ind))*'0' + str_ind + ".jpg", img)
 			cv2.imshow(img_name, img)
 			if cv2.waitKey(int(100.0 / fps)) & 0xFF == ord('q'):
 				break
+
 		else:
 			tries += 1
 		t0 = t1
@@ -91,3 +114,5 @@ while tries < 10:
 if tries == 10:
 	print("Error: no feedback from video")
 cv2.destroyAllWindows()
+df = pd.DataFrame(sa_lst, columns=["img_name", "steering_angle"])
+df.to_csv(data_dir + 'driving_log.csv', index=False)
